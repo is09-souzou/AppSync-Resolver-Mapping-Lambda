f39b07877613ab062f6c13bf11d69b42a96f6f43
ren hashimoto <hashiserver@gmail.com>
2018-06-18 18:15:32 +0900

    desciption > description

--- src/handle/work/common.go
+++ src/handle/work/common.go
@@ -2,11 +2,11 @@ package work
 
 // User type
 type Work struct {
-	ID         string   `json:"id"`
-	UserID     string   `json:"userId"`
-	Tags       []string `json:"tags`
-	CreatedAt  int      `json:"createdAt"`
-	Title      string   `json:"title"`
-	ImageUri   string   `json:"imageUri"`
-	Desciption string   `json:"description"`
+	ID          string   `json:"id"`
+	UserID      string   `json:"userId"`
+	Tags        []string `json:"tags`
+	CreatedAt   int      `json:"createdAt"`
+	Title       string   `json:"title"`
+	ImageUri    string   `json:"imageUri"`
+	Description string   `json:"description"`
 }
