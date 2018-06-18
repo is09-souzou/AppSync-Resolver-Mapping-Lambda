ed7fc9e009b17f885fbf9fdb8cc43f20eb9005e3
ren hashimoto <hashiserver@gmail.com>
2018-06-18 17:50:44 +0900

    workupdate-common fix

--- src/handle/work/common.go
+++ src/handle/work/common.go
@@ -2,7 +2,9 @@ package work
 
 // User type
 type Work struct {
-	ID    string `json:"id"`
-	Email string `json:"email"`
-	Name  string `json:"name"`
+	ID         string `json:"id"`
+	UserID     string `json:"userid"`
+	Title      string `json:"title"`
+	ImageUri   string `json:"imageuri"`
+	Desciption string `json:"description"`
 }
