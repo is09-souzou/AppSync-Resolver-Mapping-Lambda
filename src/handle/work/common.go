be9b093bbe66a7b7f9fd7ec3310a17348e3e0b3e
ren hashimoto <hashiserver@gmail.com>
2018-06-18 18:06:32 +0900

    common.go update

--- src/handle/work/common.go
+++ src/handle/work/common.go
@@ -2,9 +2,11 @@ package work
 
 // User type
 type Work struct {
-	ID         string `json:"id"`
-	UserID     string `json:"userid"`
-	Title      string `json:"title"`
-	ImageUri   string `json:"imageuri"`
-	Desciption string `json:"description"`
+	ID         string   `json:"id"`
+	UserID     string   `json:"userId"`
+	Tags       []string `json:"tags`
+	CreatedAt  int      `json:"createdAt"`
+	Title      string   `json:"title"`
+	ImageUri   string   `json:"imageuri"`
+	Desciption string   `json:"description"`
 }
