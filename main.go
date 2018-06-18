963ff04231c7dc10999fc210aedfe241191daab5
ren hashimoto <hashiserver@gmail.com>
2018-06-18 15:14:42 +0900

    workupdate add

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/is09-souzou/AppSync-Resolver-Mapping-Lambda/src/handle"
-	"encoding/json"
 )
 
 type payload struct {
-	Field     string `json:"field"`
+	Field     string          `json:"field"`
 	Arguments json.RawMessage `json:"arguments"`
 }
 
 func router(payload payload) (interface{}, error) {
 	switch payload.Field {
-		case "deleteUser":
-			var p handle.DeleteUser
-			json.Unmarshal(payload.Arguments, &p)
-			return handle.deleteUserHandle(p)
+	case "deleteUser":
+		var p handle.DeleteUser
+		json.Unmarshal(payload.Arguments, &p)
+		return handle.deleteUserHandle(p)
+	case "updateWork":
+		var p handle.UpdateWork
+		json.Unmarshal(payload.Arguments, &p)
+		return handle.updateWorkHnadle(p)
 	}
 	return nil, errors.New("field is not found")
 }
