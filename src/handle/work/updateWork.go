865d3d464cfecb27dfa43462966b5e92f03acb19
ren hashimoto <hashiserver@gmail.com>
2018-06-19 11:03:03 +0900

    workupdate fix

--- src/handle/work/updateWork.go
+++ src/handle/work/updateWork.go
@@ -10,7 +10,7 @@ import (
 
 // UpdateWork type
 type UpdateWork struct {
-	ID string `json:"id"`
+	Work Work `json:"word"`
 }
 
 // UpdateWorkHandle Update Work Handle
@@ -20,7 +20,7 @@ func UpdateWorkHandle(arg UpdateWork) (interface{}, error) {
 		&aws.Config{Region: aws.String("ap-northeast-1")},
 	)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	svc := dynamodb.New(session)
@@ -28,23 +28,35 @@ func UpdateWorkHandle(arg UpdateWork) (interface{}, error) {
 	input := &dynamodb.UpdateItemInput{
 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
 			"id": {
-				S: aws.String(arg.ID),
+				S: aws.String(""),
+			},
+			"userId": {
+				S: aws.String("hashiserver@gmail.com"),
 			},
 		},
 		TableName: aws.String("portal-works"),
 		Key: map[string]*dynamodb.AttributeValue{
-			"userId": {
-				S: aws.String(arg.ID),
+			"title": {
+				S: aws.String("test"),
+			},
+			"tags": {
+				SS: aws.StringSlice(arg.Work.Tags),
+			},
+			"imageUri": {
+				S: aws.String("https://create"),
+			},
+			"description": {
+				S: aws.String("testCode"),
 			},
 		},
 		ReturnValues:     aws.String("UPDATED_NEW"),
-		UpdateExpression: aws.String("set info.rating = :r"),
+		UpdateExpression: aws.String(""),
 	}
 
 	_, err = svc.UpdateItem(input)
 
 	if err != nil {
-		fmt.Println("Got error calling UpdateItem")
+		fmt.Println("Got error calling UpdateItem:")
 		fmt.Println(err.Error())
 		return nil, err
 	}
