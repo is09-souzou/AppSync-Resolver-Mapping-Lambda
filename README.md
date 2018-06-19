c394e12c0b8506141f721f00f2bb994192ce1007
杉山希望 <rioc.sugiyama@gmail.com>
2018-06-19 13:52:00 +0900

    add user model

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ $ zip handler.zip ./main
 ```
 GOOS=linux GOARCH=amd64 go build -o main
 zip handler.zip ./main
+mv handler.zip ~/Desktop
 ```
--- src/model/user.go
+++ src/model/user.go
@@ -1,10 +1,12 @@
 package model
 
 import (
+	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+
 const UserTableName = "portal-users"
 
 type User struct {
@@ -72,9 +74,9 @@ func GetUserList() ([]User, error) {
 
 // UpdateUserById Update user list By ID to DynamoDB
 func UpdateUserById(
-	id    *string,
+	id *string,
 	email *string,
-	name  *string,
+	name *string,
 ) error {
 
 	svc, err := getSVC()
@@ -83,22 +85,30 @@ func UpdateUserById(
 		return err
 	}
 
-	// var key map[string]
+	if (id == nil && email == nil && name == nil) {
+		return errors.New("required new value")
+	}
 
-	// if (id != nil) {
-		
-	// }
+
+	var key = map[string]*dynamodb.AttributeValue{}
+
+	if (id != nil) {
+		key["id"].S = aws.String(*id)
+	}
+
+	if (email != nil) {
+		key["email"].S = aws.String(*email)
+	}
+
+	if (name != nil) {
+		key["name"].S = aws.String(*name)
+	}
 
 	input := &dynamodb.UpdateItemInput{
-		TableName: aws.String(UserTableName),
-		Key: map[string]*dynamodb.AttributeValue{
-			"year": {
-				N: aws.String("2015"),
-			},
-			"title": {
-				S: aws.String("The Big New Movie"),
-			},
-		},
+		TableName:        aws.String(UserTableName),
+		Key:              key,
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String(""),
 	}
 
 	_, err = svc.UpdateItem(input)
@@ -109,9 +119,3 @@ func UpdateUserById(
 
 	return nil
 }
-
-// func test() {
-// 	s := "Hello, World"   
-// 	a := &s
-// 	UpdateUserById(a, nil, nil)
-// }
