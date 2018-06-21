daf2e79ded411eeefb8cd1d323d944bd1fa1307a
杉山希望 <rioc.sugiyama@gmail.com>
2018-06-21 17:13:10 +0900

    fix work handle

--- src/handle/work/common.go
+++ src/handle/work/common.go
@@ -2,7 +2,7 @@ package work
 
 // Work response work struct
 type Work struct {
-	ID          string    `json:id`
+	ID          string    `json:"id"`
 	UserID      string    `json:"userId"`
 	Title       string    `json:"title"`
 	Tags        *[]string `json:"tags"`
