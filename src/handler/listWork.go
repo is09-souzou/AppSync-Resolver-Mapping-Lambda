f1fafdec784ffb3cc67db287045dba2f3067eda5
杉山希望 <rioc.sugiyama@gmail.com>
2018-07-07 15:35:20 +0900

    feat: works search by tags func

--- src/handler/listWork.go
+++ src/handler/listWork.go
@@ -38,9 +38,7 @@ func ListWorkHandle(arg ListWork, identity types.Identity) (WorkConnection, erro
 
 	var workList model.ScanWorkListResult
 	if (arg.Option != nil && arg.Option.Tags != nil) {
-		fmt.Print("tags", arg.Option.Tags)
 		workList, err = model.ScanWorkListByTags(svc, limit, arg.ExclusiveStartKey, *arg.Option.Tags)
-		fmt.Print("result", workList)
 	} else {
 		workList, err = model.ScanWorkList(svc, limit, arg.ExclusiveStartKey)
 	}
